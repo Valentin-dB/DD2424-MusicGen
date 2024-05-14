87aaf4f838589c98e0cbffa84efbbb9da476656d
valentinbassom <valentinbassom@gmail.com>
2024-05-14 17:30:53 +0200

    Multi-feature Model

--- PreProcessor.py
+++ PreProcessor.py
@@ -47,7 +47,7 @@ def files_to_songs(files,songs={}):
             try:
                 midi_file = mido.MidiFile(file)
             except:
-                print(file)
+                print("Couldn't load :",file)
                 continue
             ticks_per_beat = midi_file.ticks_per_beat
             matrix = []
@@ -57,16 +57,21 @@ def files_to_songs(files,songs={}):
             songs[file] = np.array(matrix)
     return songs
 
-def dicts_from_songs(songs):
+def dicts_from_songs(songs,velocities_and_ticks=False):
     Channels = set()
     Notes = set()
-    Velocities = set()
-    Ticks = set()
-    for song in songs.values():
-        Channels.update(song[:,0])
-        Notes.update(song[:,1])
-        Velocities.update(song[:,2])
-        Ticks.update(song[:,3])
+    if velocities_and_ticks:
+        Velocities = set()
+        Ticks = set()
+        for song in songs.values():
+            Channels.update(song[:,0])
+            Notes.update(song[:,1])
+            Velocities.update(song[:,2])
+            Ticks.update(song[:,3])
+    else:
+        for song in songs.values():
+            Channels.update(song[:,0])
+            Notes.update(song[:,1])
         
     channel_to_ind = {}
     ind_to_channel = {}
@@ -79,19 +84,56 @@ def dicts_from_songs(songs):
     for i, note in enumerate(sorted(Notes)):
         note_to_ind[note] = i
         ind_to_note[i] = note
-    
-    velocity_to_ind = {}
-    ind_to_velocity = {}
-    for i, velocity in enumerate(sorted(Velocities)):
-        velocity_to_ind[velocity] = i
-        ind_to_velocity[i] = velocity
         
-    tick_to_ind = {}
-    ind_to_tick = {}
-    for i, tick in enumerate(sorted(Ticks)):
-        tick_to_ind[tick] = i
-        ind_to_tick[i] = tick
-    return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, velocity_to_ind, ind_to_velocity, tick_to_ind, ind_to_tick
+    if velocities_and_ticks:
+        velocity_to_ind = {}
+        ind_to_velocity = {}
+        for i, velocity in enumerate(sorted(Velocities)):
+            velocity_to_ind[velocity] = i
+            ind_to_velocity[i] = velocity
+
+        tick_to_ind = {}
+        ind_to_tick = {}
+        for i, tick in enumerate(sorted(Ticks)):
+            tick_to_ind[tick] = i
+            ind_to_tick[i] = tick
+        return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, velocity_to_ind, ind_to_velocity, tick_to_ind, ind_to_tick
+    return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note
+
+def ranges_from_songs(songs,channels_and_notes=False):
+    range_velocities = [np.inf,-np.inf]
+    range_ticks = [np.inf,-np.inf]
+    if channels_and_notes:
+        range_channels = [np.inf,-np.inf]
+        range_notes = [np.inf,-np.inf]
+        for song in songs.values():
+            min_chan = min(song[:,0])
+            max_chan = max(song[:,0])
+            min_note = min(song[:,1])
+            max_note = max(song[:,1])
+            min_vel = min(song[:,2])
+            max_vel = max(song[:,2])
+            min_tick = min(song[:,3])
+            max_tick = max(song[:,3])
+            if min_chan < range_channels[0] : range_channels[0] = min_chan
+            if max_chan > range_channels[1] : range_channels[1] = max_chan
+            if min_note < range_notes[0] : range_notes[0] = min_note
+            if max_note > range_notes[1] : range_notes[1] = max_note
+            if min_vel < range_velocities[0] : range_velocities[0] = min_vel
+            if max_vel > range_velocities[1] : range_velocities[1] = max_vel
+            if min_tick < range_ticks[0] : range_ticks[0] = min_tick
+            if max_tick > range_ticks[1] : range_ticks[1] = max_tick
+        return range_channels, range_notes, range_velocities, range_ticks
+    for song in songs.values():
+        min_vel = min(song[:,2])
+        max_vel = max(song[:,2])
+        min_tick = min(song[:,3])
+        max_tick = max(song[:,3])
+        if min_vel < range_velocities[0] : range_velocities[0] = min_vel
+        if max_vel > range_velocities[1] : range_velocities[1] = max_vel
+        if min_tick < range_ticks[0] : range_ticks[0] = min_tick
+        if max_tick > range_ticks[1] : range_ticks[1] = max_tick
+    return range_velocities, range_ticks
 
 def one_hot_encode(char_to_ind,sequence):
     k = len(char_to_ind)
