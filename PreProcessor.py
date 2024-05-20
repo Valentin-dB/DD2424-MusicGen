2d56ea532b3c591543020ba8667e6c83899ab31b
valentinbassom <valentinbassom@gmail.com>
2024-05-20 15:22:53 +0200

    Comparison

--- PreProcessor.py
+++ PreProcessor.py
@@ -56,21 +56,16 @@ def files_to_songs(files,songs={}):
             songs[file] = np.array(matrix)
     return songs
 
-def dicts_from_songs(songs,velocities_and_times=False):
+def dicts_from_songs(songs,velocities=True,times=False):
     Channels = set()
     Notes = set()
-    if velocities_and_times:
-        Velocities = set()
-        Times = set()
-        for song in songs.values():
-            Channels.update(song[:,0])
-            Notes.update(song[:,1])
-            Velocities.update(song[:,2])
-            Times.update(song[:,3])
-    else:
-        for song in songs.values():
-            Channels.update(song[:,0])
-            Notes.update(song[:,1])
+    if velocities : Velocities = set()
+    if times : Times = set()
+    for song in songs.values():
+        Channels.update(song[:,0])
+        Notes.update(song[:,1])
+        if velocities : Velocities.update(song[:,2])
+        if times : Times.update(song[:,3])
         
     channel_to_ind = {}
     ind_to_channel = {}
@@ -84,55 +79,63 @@ def dicts_from_songs(songs,velocities_and_times=False):
         note_to_ind[int(note)] = i
         ind_to_note[i] = int(note)
         
-    if velocities_and_times:
+    if velocities :
         velocity_to_ind = {}
         ind_to_velocity = {}
         for i, velocity in enumerate(sorted(Velocities)):
             velocity_to_ind[int(velocity)] = i
             ind_to_velocity[i] = int(velocity)
-
+    if times : 
         time_to_ind = {}
         ind_to_time = {}
         for i, time in enumerate(sorted(Times)):
             time_to_ind[time] = i
             ind_to_time[i] = time
-        return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, velocity_to_ind, ind_to_velocity, time_to_ind, ind_to_time
+    if velocities and times : return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, velocity_to_ind, ind_to_velocity, time_to_ind, ind_to_time
+    if velocities : return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, velocity_to_ind, ind_to_velocity
+    if times : return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note, time_to_ind, ind_to_time
     return channel_to_ind, ind_to_channel, note_to_ind, ind_to_note
 
-def ranges_from_songs(songs,channels_and_notes=False):
-    range_velocities = [np.inf,-np.inf]
-    range_times = [np.inf,-np.inf]
-    if channels_and_notes:
+def ranges_from_songs(songs,channels=False,notes=False,velocities=False,times=True):
+    res = ()
+    n_true = channels+notes+velocities+times
+    if channels :
         range_channels = [np.inf,-np.inf]
-        range_notes = [np.inf,-np.inf]
         for song in songs.values():
             min_chan = min(song[:,0])
             max_chan = max(song[:,0])
-            min_note = min(song[:,1])
-            max_note = max(song[:,1])
-            min_vel = min(song[:,2])
-            max_vel = max(song[:,2])
-            min_time = min(song[:,3])
-            max_time = max(song[:,3])
             if min_chan < range_channels[0] : range_channels[0] = min_chan
             if max_chan > range_channels[1] : range_channels[1] = max_chan
+        if n_true == 1 : return [int(a) for a in range_channels]
+        res += ([int(a) for a in range_channels],)
+    if notes :
+        range_notes = [np.inf,-np.inf]
+        for song in songs.values():
+            min_note = min(song[:,1])
+            max_note = max(song[:,1])
             if min_note < range_notes[0] : range_notes[0] = min_note
             if max_note > range_notes[1] : range_notes[1] = max_note
+        if n_true == 1 : return [int(a) for a in range_notes]
+        res += ([int(a) for a in range_notes],)
+    if velocities :
+        range_velocities = [np.inf,-np.inf]
+        for song in songs.values():
+            min_vel = min(song[:,2])
+            max_vel = max(song[:,2])
             if min_vel < range_velocities[0] : range_velocities[0] = min_vel
             if max_vel > range_velocities[1] : range_velocities[1] = max_vel
+        if n_true == 1 : return [int(a) for a in range_velocities]
+        res += ([int(a) for a in range_velocities],)
+    if times :
+        range_times = [np.inf,-np.inf]
+        for song in songs.values():
+            min_time = min(song[:,3])
+            max_time = max(song[:,3])
             if min_time < range_times[0] : range_times[0] = min_time
             if max_time > range_times[1] : range_times[1] = max_time
-        return [int(a) for a in range_channels], [int(a) for a in range_notes], [int(a) for a in range_velocities], range_times
-    for song in songs.values():
-        min_vel = min(song[:,2])
-        max_vel = max(song[:,2])
-        min_time = min(song[:,3])
-        max_time = max(song[:,3])
-        if min_vel < range_velocities[0] : range_velocities[0] = min_vel
-        if max_vel > range_velocities[1] : range_velocities[1] = max_vel
-        if min_time < range_times[0] : range_times[0] = min_time
-        if max_time > range_times[1] : range_times[1] = max_time
-    return [int(a) for a in range_velocities], range_times
+        if n_true == 1 : return range_times
+        res += (range_times,)
+    return res
 
 def one_hot_encode(char_to_ind,sequence):
     k = len(char_to_ind)
