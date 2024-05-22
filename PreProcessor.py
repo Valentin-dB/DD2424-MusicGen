5fcf348708b77b2823dc2fd1175d6402dcebae0a
valentinbassom <valentinbassom@gmail.com>
2024-05-22 15:03:39 +0200

    Transformer, train unfinished

--- PreProcessor.py
+++ PreProcessor.py
@@ -157,3 +157,144 @@ def label_sequences(one_hot,sequence_length,n_batch=1):
     training_set = np.reshape(one_hot[:n_samples*sequence_length,:],(n_samples,sequence_length,k))
     label_set = np.reshape(one_hot[1:n_samples*sequence_length+1,:],(n_samples,sequence_length,k))
     return training_set, label_set
+
+def label_sequences_transformer(x,sequence_length,char_to_ind=None,n_batch=1):
+    n = x.shape[0]
+    n_samples = (n-1)//sequence_length
+    n_samples = (n_samples//n_batch)*n_batch
+    training_set = np.reshape(x[:n_samples*sequence_length],(n_samples,sequence_length))
+    if char_to_ind is not None :
+        x = one_hot_encode(char_to_ind,x)
+        label_set = np.reshape(x[1:n_samples*sequence_length+1,:],(n_samples,sequence_length,-1))
+    else :
+        label_set = np.reshape(x[1:n_samples*sequence_length+1],(n_samples,sequence_length,1))
+    return training_set, label_set
+
+def prep_data(seq_length,ClassicSongs,n_Channels,n_Notes,n_Velocities,channel_to_ind,note_to_ind,velocity_to_ind,val_split=0.1,test_split=0.1):
+    if type(ClassicSongs) == list : ClassicSongs = files_to_songs(ClassicSongs)
+        
+    total_samples = sum((len(song) - 1) // seq_length for song in ClassicSongs.values())
+
+    X_Channels = np.zeros((total_samples, seq_length, n_Channels))
+    X_Notes = np.zeros((total_samples, seq_length, n_Notes))
+    X_Velocities = np.zeros((total_samples, seq_length, n_Velocities))
+    X_Times = np.zeros((total_samples, seq_length, 1))
+    y_Channels = np.zeros((total_samples, seq_length, n_Channels))
+    y_Notes = np.zeros((total_samples, seq_length, n_Notes))
+    y_Velocities = np.zeros((total_samples, seq_length, n_Velocities))
+    y_Times = np.zeros((total_samples, seq_length, 1))
+
+    current_index = 0
+    for song in ClassicSongs.values():
+        song_x_channels, song_y_channels = label_sequences(one_hot_encode(channel_to_ind, song[:, 0]), seq_length)
+        song_x_notes, song_y_notes = label_sequences(one_hot_encode(note_to_ind, song[:, 1]), seq_length)
+        song_x_velocities, song_y_velocities = label_sequences(one_hot_encode(velocity_to_ind, song[:, 2]), seq_length)
+        song_x_ticks, song_y_ticks = label_sequences(song[:, 3:], seq_length)
+        
+        n_samples = song_x_channels.shape[0]
+        next_index = current_index + n_samples
+        
+        X_Channels[current_index:next_index] = song_x_channels
+        X_Notes[current_index:next_index] = song_x_notes
+        X_Velocities[current_index:next_index] = song_x_velocities
+        X_Times[current_index:next_index] = song_x_ticks
+        y_Channels[current_index:next_index] = song_y_channels
+        y_Notes[current_index:next_index] = song_y_notes
+        y_Velocities[current_index:next_index] = song_y_velocities
+        y_Times[current_index:next_index] = song_y_ticks
+        
+        current_index = next_index
+
+    train_split = 1 - val_split - test_split
+
+    n_val = int(total_samples*val_split)
+    n_test = int(total_samples*test_split)
+    indices = np.random.permutation(total_samples)
+    val_indices = indices[:n_val]
+    test_indices = indices[-n_test:]
+    train_indices = indices[n_val:-n_test]
+
+    Test_X_Channels = X_Channels[test_indices,:,:]
+    Test_X_Notes = X_Notes[test_indices,:,:]
+    Test_X_Velocities = X_Velocities[test_indices,:,:]
+    Test_X_Times = X_Times[test_indices,:,:]
+    Test_y_Channels = y_Channels[test_indices,:,:]
+    Test_y_Notes = y_Notes[test_indices,:,:]
+    Test_y_Velocities = y_Velocities[test_indices,:,:]
+    Test_y_Times = y_Times[test_indices,:,:]
+
+    Val_X_Channels = X_Channels[val_indices,:,:]
+    Val_X_Notes = X_Notes[val_indices,:,:]
+    Val_X_Velocities = X_Velocities[val_indices,:,:]
+    Val_X_Times = X_Times[val_indices,:,:]
+    Val_y_Channels = y_Channels[val_indices,:,:]
+    Val_y_Notes = y_Notes[val_indices,:,:]
+    Val_y_Velocities = y_Velocities[val_indices,:,:]
+    Val_y_Times = y_Times[val_indices,:,:]
+
+
+    X_Channels = X_Channels[train_indices,:,:]
+    X_Notes = X_Notes[train_indices,:,:]
+    X_Velocities = X_Velocities[train_indices,:,:]
+    X_Times = X_Times[train_indices,:,:]
+    y_Channels = y_Channels[train_indices,:,:]
+    y_Notes = y_Notes[train_indices,:,:]
+    y_Velocities = y_Velocities[train_indices,:,:]
+    y_Times = y_Times[train_indices,:,:]
+
+    return X_Channels, X_Notes, X_Velocities, X_Times, y_Channels, y_Notes, y_Velocities, y_Times, Val_X_Channels, Val_X_Notes, Val_X_Velocities, Val_X_Times, Val_y_Channels, Val_y_Notes, Val_y_Velocities, Val_y_Times, Test_X_Channels, Test_X_Notes, Test_X_Velocities, Test_X_Times, Test_y_Channels, Test_y_Notes, Test_y_Velocities, Test_y_Times
+
+def prep_data_transformer(seq_length,ClassicSongs,n_Channels,n_Notes,channel_to_ind,note_to_ind,val_split=0.1,test_split=0.1):
+    if type(ClassicSongs) == list : ClassicSongs = files_to_songs(ClassicSongs)
+        
+    total_samples = sum((len(song) - 1) // seq_length for song in ClassicSongs.values())
+
+    X_Channels = np.zeros((total_samples, seq_length))
+    X_Notes = np.zeros((total_samples, seq_length))
+    X_Velocities = np.zeros((total_samples, seq_length))
+    X_Times = np.zeros((total_samples, seq_length))
+    y_Channels = np.zeros((total_samples, seq_length, n_Channels))
+    y_Notes = np.zeros((total_samples, seq_length, n_Notes))
+    y_Velocities = np.zeros((total_samples, seq_length, 1))
+    y_Times = np.zeros((total_samples, seq_length, 1))
+
+    current_index = 0
+    for song in ClassicSongs.values():
+        song_x_channels, song_y_channels = label_sequences_transformer(song[:, 0],seq_length, channel_to_ind)
+        song_x_notes, song_y_notes = label_sequences_transformer(song[:, 1], seq_length, note_to_ind)
+        song_x_velocities, song_y_velocities = label_sequences_transformer(song[:, 2], seq_length)
+        song_x_ticks, song_y_ticks = label_sequences_transformer(song[:, 3], seq_length)
+        
+        n_samples = song_x_channels.shape[0]
+        next_index = current_index + n_samples
+        
+        X_Channels[current_index:next_index] = song_x_channels
+        X_Notes[current_index:next_index] = song_x_notes
+        X_Velocities[current_index:next_index] = song_x_velocities
+        X_Times[current_index:next_index] = song_x_ticks
+        y_Channels[current_index:next_index] = song_y_channels
+        y_Notes[current_index:next_index] = song_y_notes
+        y_Velocities[current_index:next_index] = song_y_velocities
+        y_Times[current_index:next_index] = song_y_ticks
+        
+        current_index = next_index
+
+    train_split = 1 - val_split - test_split
+
+    n_val = int(total_samples*val_split)
+    n_test = int(total_samples*test_split)
+    indices = np.random.permutation(total_samples)
+    val_indices = indices[:n_val]
+    test_indices = indices[-n_test:]
+    train_indices = indices[n_val:-n_test]
+
+    Test_X = [X_Channels[test_indices,:],X_Notes[test_indices,:],X_Velocities[test_indices,:],X_Times[test_indices,:]]
+    Test_y = [y_Channels[test_indices,:,:],y_Notes[test_indices,:,:],y_Velocities[test_indices,:],y_Times[test_indices,:]]
+
+    Val_X = [X_Channels[val_indices,:],X_Notes[val_indices,:],X_Velocities[val_indices,:],X_Times[val_indices,:]]
+    Val_y = [y_Channels[val_indices,:,:],y_Notes[val_indices,:,:],y_Velocities[val_indices,:],y_Times[val_indices,:]]
+
+    Train_X = [X_Channels[train_indices,:],X_Notes[train_indices,:],X_Velocities[train_indices,:],X_Times[train_indices,:]]
+    Train_y = [y_Channels[train_indices,:,:],y_Notes[train_indices,:,:],y_Velocities[train_indices,:],y_Times[train_indices,:]]
+
+    return Train_X, Train_y, Val_X, Val_y, Test_X, Test_y
