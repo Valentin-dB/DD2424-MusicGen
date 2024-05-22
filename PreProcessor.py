479789ffd12460038d1627606f6d6f503feb52d1
valentinbassom <valentinbassom@gmail.com>
2024-05-22 18:43:45 +0200

    Transformer generate

--- PreProcessor.py
+++ PreProcessor.py
@@ -150,6 +150,18 @@ def one_hot_encode(char_to_ind,sequence):
             one_hot[i][char_to_ind[sequence[i]]] = 1
     return one_hot
 
+def one_hot_encode_no_dict(sequence,k):
+    if type(sequence) == int:
+        one_hot = np.zeros(k)
+        one_hot[sequence] = 1
+        return one_hot
+    else :
+        n = len(sequence)
+        one_hot = np.zeros((n, k))
+        for i in range(n):
+            one_hot[i][sequence[i]] = 1
+    return one_hot
+
 def label_sequences(one_hot,sequence_length,n_batch=1):
     n,k = one_hot.shape
     n_samples = (n-1)//sequence_length
@@ -158,13 +170,13 @@ def label_sequences(one_hot,sequence_length,n_batch=1):
     label_set = np.reshape(one_hot[1:n_samples*sequence_length+1,:],(n_samples,sequence_length,k))
     return training_set, label_set
 
-def label_sequences_transformer(x,sequence_length,char_to_ind=None,n_batch=1):
+def label_sequences_transformer(x,sequence_length,to_one_hot=None,n_batch=1):
     n = x.shape[0]
     n_samples = (n-1)//sequence_length
     n_samples = (n_samples//n_batch)*n_batch
     training_set = np.reshape(x[:n_samples*sequence_length],(n_samples,sequence_length))
-    if char_to_ind is not None :
-        x = one_hot_encode(char_to_ind,x)
+    if to_one_hot is not None :
+        x = one_hot_encode_no_dict(x,to_one_hot)
         label_set = np.reshape(x[1:n_samples*sequence_length+1,:],(n_samples,sequence_length,-1))
     else :
         label_set = np.reshape(x[1:n_samples*sequence_length+1],(n_samples,sequence_length,1))
@@ -244,7 +256,7 @@ def prep_data(seq_length,ClassicSongs,n_Channels,n_Notes,n_Velocities,channel_to
 
     return X_Channels, X_Notes, X_Velocities, X_Times, y_Channels, y_Notes, y_Velocities, y_Times, Val_X_Channels, Val_X_Notes, Val_X_Velocities, Val_X_Times, Val_y_Channels, Val_y_Notes, Val_y_Velocities, Val_y_Times, Test_X_Channels, Test_X_Notes, Test_X_Velocities, Test_X_Times, Test_y_Channels, Test_y_Notes, Test_y_Velocities, Test_y_Times
 
-def prep_data_transformer(seq_length,ClassicSongs,n_Channels,n_Notes,channel_to_ind,note_to_ind,val_split=0.1,test_split=0.1):
+def prep_data_transformer(seq_length,ClassicSongs,n_Channels,n_Notes,n_Velocities,val_split=0.1,test_split=0.1):
     if type(ClassicSongs) == list : ClassicSongs = files_to_songs(ClassicSongs)
         
     total_samples = sum((len(song) - 1) // seq_length for song in ClassicSongs.values())
@@ -260,9 +272,9 @@ def prep_data_transformer(seq_length,ClassicSongs,n_Channels,n_Notes,channel_to_
 
     current_index = 0
     for song in ClassicSongs.values():
-        song_x_channels, song_y_channels = label_sequences_transformer(song[:, 0],seq_length, channel_to_ind)
-        song_x_notes, song_y_notes = label_sequences_transformer(song[:, 1], seq_length, note_to_ind)
-        song_x_velocities, song_y_velocities = label_sequences_transformer(song[:, 2], seq_length)
+        song_x_channels, song_y_channels = label_sequences_transformer(song[:, 0].astype(int), seq_length, n_Channels)
+        song_x_notes, song_y_notes = label_sequences_transformer(song[:, 1].astype(int), seq_length, n_Notes)
+        song_x_velocities, song_y_velocities = label_sequences_transformer(song[:, 2].astype(int), seq_length)
         song_x_ticks, song_y_ticks = label_sequences_transformer(song[:, 3], seq_length)
         
         n_samples = song_x_channels.shape[0]
